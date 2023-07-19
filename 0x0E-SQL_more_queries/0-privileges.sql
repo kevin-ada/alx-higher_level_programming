ec09455b8b8d83b3b360d86a772c5b7e23ae7fd5
kim-thuita <kevinkoech265@gmail.com>
2023-07-19 16:36:57 -0700

    fixes for task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---  script that lists all privileges of the MySQL users
+--  script that lists all privileges of two users
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
