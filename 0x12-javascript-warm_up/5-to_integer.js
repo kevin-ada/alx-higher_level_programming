214d507c01b885d754852bc31b746ade928fbdde
kevin-ada <kevinkoech265@gmail.com>
2023-08-14 20:05:28 +0000

    int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-let num = process.argv[2];
+const num = process.argv[2];
 
-
-
-if (!isNaN(num)){
-  console.log('My number: ',parseInt(num));
+if (!isNaN(num)) {
+  console.log('My number: ' + parseInt(num));
 } else {
   console.log('Not a number');
 }
