cdd255b4d60bb31d570f2ce7e3ae997ebadf707a
kim-thuita <kevinkoech265@gmail.com>
2023-08-14 19:51:25 +0300

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const process = require("process");
+
+const argv = process.argv;
+
+const argsCount = argv.length;
+
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
