4466717f40e9ce7277294d79482c8f8284b299da
kevin-ada <kevinkoech265@gmail.com>
2023-08-14 21:23:46 +0000

    fixes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 // Arrays
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
