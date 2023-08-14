03113f1ec6c5186f76bda2005610174d38706d37
kim-thuita <kevinkoech265@gmail.com>
2023-08-14 19:52:28 +0300

    fixesmade to accept linter

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const process = require("process");
+const process = require('process');
 
 const argv = process.argv;
 
 const argsCount = argv.length;
 
 if (argsCount === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argsCount === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
