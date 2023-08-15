f2128f68f0faa609f5a9d50659ff01d55a58ddec
kim-thuita <kevinkoech265@gmail.com>
2023-08-16 01:25:59 +0300

    semistandard fixes

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-      this.width = w; // set width property to w
-      this.height = h; // set height property to h
+    this.width = w; // set width property to w
+    this.height = h; // set height property to h
   }
-}
+};
