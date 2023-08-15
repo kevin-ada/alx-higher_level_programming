80e45fbfe8482612dd1c140396d43689d5ef5a0d
kim-thuita <kevinkoech265@gmail.com>
2023-08-16 01:22:25 +0300

    "task-1 implementation"

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+      this.width = w; // set width property to w
+      this.height = h; // set height property to h
+  }
+}
