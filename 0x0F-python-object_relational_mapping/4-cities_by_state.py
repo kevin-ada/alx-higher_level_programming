15db8dd931d1444c2fc7fc46ee653cfeb19c59ac
kim-thuita <kevinkoech265@gmail.com>
2023-08-20 03:03:17 -0700

    cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that lists all states from the database using MySQL
+"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    cursor = db.cursor()  # cursor object is used 4 execution of sql queries
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC;")
+    mydata = cursor.fetchall()
+
+    for row in mydata:
+        print(row)
+
+    cursor.close()
+    db.close()
+
