5c78bbf720a8ed743c30e61146568a8c8744095e
kim-thuita <kevinkoech265@gmail.com>
2023-08-20 18:31:37 +0300

    "class State models "

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,29 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """
-Module that lists all states from the database using MySQL
-"""
+ 3 Module that lists all states from the database using MySQL
+ 4 """
 
 if __name__ == '__main__':
-
     import MySQLdb
     import sys
 
     db = MySQLdb.connect(
         host='localhost',
+
         port=3306,
+
         user=sys.argv[1],
+
         passwd=sys.argv[2],
-        db=sys.argv[3]
-        )
 
+        db=sys.argv[3])
     cursor = db.cursor()  # cursor object is used 4 execution of sql queries
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+             INNER JOIN states on state_id = states.id \
+            ORDER BY cities.id ASC;")
     mydata = cursor.fetchall()
-
     for row in mydata:
         print(row)
-
     cursor.close()
+
     db.close()
