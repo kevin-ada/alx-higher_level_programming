817d3a139abf1361c9b38dd421cf57280096b381
kim-thuita <kevinkoech265@gmail.com>
2023-09-02 01:07:27 +0300

    "Error code #1"

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,6 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    print(response.content)
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
