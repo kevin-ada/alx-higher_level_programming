7fb002c454aaba8fed6aca4fd471c5f630e57080
kim-thuita <kevinkoech265@gmail.com>
2023-09-01 20:14:08 +0300

    "Implementing task 0-hbtn"

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """Fetches https://intranet.hbtn.io/status"""
 from urllib import request
 
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(content)))
+        print("\t - content: {}".format(content))
+        print("\t - utf8 content: {}".format(content.decode('utf-8')))
