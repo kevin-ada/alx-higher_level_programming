294abb5f07416d84df44ac46a3f0627ba8668796
kim-thuita <kevinkoech265@gmail.com>
2023-09-01 21:33:45 +0300

    "Error on task 3"

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,9 +3,10 @@
 
 from urllib import request, error
 import sys
+
 if __name__ == "__main__":
-    with request.urlopen(sys.argv[1]) as response:
-        try:
+    try:
+        with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
