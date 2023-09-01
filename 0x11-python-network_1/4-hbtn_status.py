b0d0573c47737eb28db408762112da495870eede
kim-thuita <kevinkoech265@gmail.com>
2023-09-01 22:29:11 +0300

    "Working with the requests module"

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.status_code)))
+    print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.content.decode('utf-8')))
