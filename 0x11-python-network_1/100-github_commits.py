1a1c12c4638562479dbc7cc7105156205c9b0ce9
kim-thuita <kevinkoech265@gmail.com>
2023-09-02 02:59:51 +0300

    "Worked on  fetching API's using the GET requests"

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,4 +9,6 @@ if __name__ == "__main__":
     response = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]))
     commits = response.json()
     for commit in commits[:10]:
-        print("{}: {}".format(commit.get('sha'), commit.get('commit').get('author').get('name')))
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').
+                              get('name')))
