b9cdef1c57eb5149c2f9d8feff3080566b32e9f0
kim-thuita <kevinkoech265@gmail.com>
2023-09-02 02:48:26 +0300

    "Worked on  fetching API's using the GET requests"

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Python script that takes your GitHub credentials (username and password)"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]))
+    commits = response.json()
+    for commit in commits[:10]:
+        print("{}: {}".format(commit.get('sha'), commit.get('commit').get('author').get('name')))
