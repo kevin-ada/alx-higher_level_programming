7117a713368b47cc2568102f878f1d6bf24817c3
kim-thuita <kevinkoech265@gmail.com>
2023-09-02 00:49:51 +0300

    "The Task-6"

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     email = {'email': sys.argv[2]}
-    response = requests.get(sys.argv[1], data=email)
+    response = requests.post(sys.argv[1], data=email)
     print(response.text)
 
 
