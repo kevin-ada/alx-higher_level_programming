db462d5becfcf3dec6aebb9b0f12bc9812efc7f2
kim-thuita <kevinkoech265@gmail.com>
2023-09-02 03:02:46 +0300

    "indetation-fixes"

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     response = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2],
-	argv[1]))
+                                                                                argv[1]))
     commits = response.json()
     for commit in commits[:10]:
         print("{}: {}".format(commit.get('sha'),
