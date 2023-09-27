f8d1f53d2f5703247d54a3fcf9721be0e3deeeb5
kim-thuita <kevinkoech265@gmail.com>
2023-09-27 19:05:42 +0300

    "This is task 2 response code"

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+request.get(process.argv[2], (error, response, body) => {
+    if (error){
+        throw error
+    }else {
+        console.log(response.statusCode)
+    }
+})
