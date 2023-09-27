ff97c84b5896a3d2ab002de540b99c29a72ecfd7
kevin-ada <kevinkoech265@gmail.com>
2023-09-27 11:28:10 -0700

    loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-const request = require('request')
-const fs = require('fs')
+const request = require('request');
+const fs = require('fs');
 
-const url = process.argv[2]
+const url = process.argv[2];
 
-request.get(url, (error, response) =>{
-    if (error){
-        throw error
-    }else {
-        fs.writeFile(process.argv[3], `${response.body}`, 'utf-8' ,(err) =>{
-            if (err){
-                throw error
-            }
-            }
-        )
+request.get(url, (error, response) => {
+  if (error) {
+    throw error;
+  } else {
+    fs.writeFile(process.argv[3], `${response.body}`, 'utf-8', (err) => {
+      if (err) {
+        throw error;
+      }
     }
-})
+    );
+  }
+});
