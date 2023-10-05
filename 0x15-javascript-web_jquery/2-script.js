f1c9ec7c01fbee4e6d5da6c7c308407eceb578b0
kim-thuita <kevinkoech265@gmail.com>
2023-10-05 11:31:39 +0300

    "using the click func in jquery"

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header:
+
+
+const click = $('DIV#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
+
+click();
